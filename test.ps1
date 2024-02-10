3fc94c1da3d6b24446b6482365a09f686a818208
NARENDRA KUMAR <87057358+narendrakumar02-nagarro@users.noreply.github.com>
2024-02-10 18:08:31 +0530

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,9 @@
+winrm set winrm/config/client '@{TrustedHosts="13.233.43.55"}'
+$pw = convertto-securestring -AsPlainText -Force -String 0Gg-u3NvIPWEpJYvyRlgerZ$%%ep4-jr
+# echo $pw
+# echo $creds
+$creds = new-object -typename System.Management.Automation.PSCredential -argumentlist "Administrator",$pw
+#echo $creds
+$sest = New-PSSession -ComputerName "13.233.43.55" -Credential $creds
+# echo $sest
+Copy-Item "C:\\sitecore\\InstallARM.ps1" -Destination "C:\\test" -ToSession $sest 
